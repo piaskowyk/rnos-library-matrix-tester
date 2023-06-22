0635c4e8d62cd6721eb802d188518cfc206dd180
Krzysztof Piaskowy <krzysztof.piaskowy@swmansion.com>
2023-06-22 15:49:56 +0200

    test

--- cli/CreateApp.sh
+++ cli/CreateApp.sh
@@ -3,4 +3,5 @@ set -u
 
 echo react-native init $E2E_APP_NAME --directory $E2E_APP_PATH --version $E2E_REACT_NATIVE_VERSION --skip-install
 pwd
-npx react-native init $E2E_APP_NAME --directory $E2E_APP_PATH --version $E2E_REACT_NATIVE_VERSION --skip-install
+# npx react-native init $E2E_APP_NAME --directory $E2E_APP_PATH --version $E2E_REACT_NATIVE_VERSION --skip-install
+npx --yes react-native init piesdzik --directory ./eee1 --version 0.72.0 --skip-install --verbose
