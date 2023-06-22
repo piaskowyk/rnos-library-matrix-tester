2230fb2967a221e4822c2af6393bec787444976f
Krzysztof Piaskowy <krzysztof.piaskowy@swmansion.com>
2023-06-22 20:38:24 +0200

    a

--- cli/CreateApp.sh
+++ cli/CreateApp.sh
@@ -4,4 +4,5 @@ set -u
 echo react-native init $E2E_APP_NAME --directory $E2E_APP_PATH --version $E2E_REACT_NATIVE_VERSION --skip-install
 pwd
 # npx react-native init $E2E_APP_NAME --directory $E2E_APP_PATH --version $E2E_REACT_NATIVE_VERSION --skip-install
-npx react-native init piesdzik
+# npx react-native init piesdzik
+npx react-native@latest init AwesomeProject
